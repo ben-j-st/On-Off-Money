0b0acf1d3be7c71ebd43335411e9bf441267e87f
Ben Stephens <ben_j_stephens@hotmail.com>
2020-09-14 14:45:18 +1000

    changed api route to db route

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/db/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
